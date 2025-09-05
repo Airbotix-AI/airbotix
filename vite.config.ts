7c6d586d737cefe53221bc44792dd66f842f7069
Jason <wangzeyu.developer@gmail.com>
2025-09-05 11:25:08 +1000

    revert: restore base path to /airbotix-website/ as requested

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-﻿import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/airbotix-website/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
